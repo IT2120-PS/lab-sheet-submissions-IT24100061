8ce64450cdf193b7d41d7a43c354951f0d27e1be
IT24100061 <IT24100061@my.sliit.lk>
2025-09-16 11:58:57 +0530

    Lab07-IT2120

---
+++ Lab Sheet 07/IT24100061/IT24100061.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\user\\Desktop\\IT24100061")
+#Question 01
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+#Question 02
+pexp(2, rate=1/3)
+
+#Question 03
+# 1
+1 - pnorm(130, mean=100, sd=15)
+
+# 2
+qnorm(0.95, mean=100, sd=15)
+
